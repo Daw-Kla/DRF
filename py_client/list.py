985385a85fc72bc0fec9871b89ccd72a35da036e
Daw-Kla <137680024+Daw-Kla@users.noreply.github.com>
2023-10-03 18:23:48 +0200

    auth token added

--- py_client/list.py
+++ py_client/list.py
@@ -1,7 +1,21 @@
 import requests
+from getpass import getpass
 
-endpoint = 'http://localhost:8000/api/products/'
+auth_endpoint = 'http://localhost:8000/api/auth/'
+username = input('What is your username?\n')
+password = getpass('What is your password\n')
 
-#listing all objects from DB
-get_response = requests.get(endpoint, verify=False) 
-print(get_response.json())
+auth_response = requests.post(auth_endpoint, json={'username': 'admin', 'password': password}) 
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headres = {
+        'Authorization': f'Bearer {token}'
+    }
+    
+    endpoint = 'http://localhost:8000/api/products/'
+
+    #listing all objects from DB
+    get_response = requests.get(endpoint, headers=headres) 
+    print(get_response.json())
